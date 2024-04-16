4327ac5a1be6816d0d2aa6c5f94528d547f218f4
Adrian Duke <adrian.duke@gmail.com>
2024-04-16 15:11:52 +0100

    remove stale test from iter

--- kv/iter/iter_test.go
+++ kv/iter/iter_test.go
@@ -22,6 +22,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
 	"github.com/uncommoncorrelation/go-mdbx-db/kv/iter"
 	"github.com/uncommoncorrelation/go-mdbx-db/kv/order"
 )
@@ -76,70 +77,6 @@ func TestUnion(t *testing.T) {
 	})
 }
 
-// func TestUnionPairs(t *testing.T) {
-// 	db := memdb.NewTestDB(t)
-// 	ctx := context.Background()
-// 	t.Run("simple", func(t *testing.T) {
-// 		require := require.New(t)
-// 		tx, _ := db.BeginRw(ctx)
-// 		defer tx.Rollback()
-// 		_ = tx.Put(kv.E2AccountsHistory, []byte{1}, []byte{1})
-// 		_ = tx.Put(kv.E2AccountsHistory, []byte{3}, []byte{1})
-// 		_ = tx.Put(kv.E2AccountsHistory, []byte{4}, []byte{1})
-// 		_ = tx.Put(kv.PlainState, []byte{2}, []byte{9})
-// 		_ = tx.Put(kv.PlainState, []byte{3}, []byte{9})
-// 		it, _ := tx.Range(kv.E2AccountsHistory, nil, nil)
-// 		it2, _ := tx.Range(kv.PlainState, nil, nil)
-// 		keys, values, err := iter.ToKVArray(iter.UnionKV(it, it2, -1))
-// 		require.NoError(err)
-// 		require.Equal([][]byte{{1}, {2}, {3}, {4}}, keys)
-// 		require.Equal([][]byte{{1}, {9}, {1}, {1}}, values)
-// 	})
-// 	t.Run("empty 1st", func(t *testing.T) {
-// 		require := require.New(t)
-// 		tx, _ := db.BeginRw(ctx)
-// 		defer tx.Rollback()
-// 		_ = tx.Put(kv.PlainState, []byte{2}, []byte{9})
-// 		_ = tx.Put(kv.PlainState, []byte{3}, []byte{9})
-// 		it, _ := tx.Range(kv.E2AccountsHistory, nil, nil)
-// 		it2, _ := tx.Range(kv.PlainState, nil, nil)
-// 		keys, _, err := iter.ToKVArray(iter.UnionKV(it, it2, -1))
-// 		require.NoError(err)
-// 		require.Equal([][]byte{{2}, {3}}, keys)
-// 	})
-// 	t.Run("empty 2nd", func(t *testing.T) {
-// 		require := require.New(t)
-// 		tx, _ := db.BeginRw(ctx)
-// 		defer tx.Rollback()
-// 		_ = tx.Put(kv.E2AccountsHistory, []byte{1}, []byte{1})
-// 		_ = tx.Put(kv.E2AccountsHistory, []byte{3}, []byte{1})
-// 		_ = tx.Put(kv.E2AccountsHistory, []byte{4}, []byte{1})
-// 		it, _ := tx.Range(kv.E2AccountsHistory, nil, nil)
-// 		it2, _ := tx.Range(kv.PlainState, nil, nil)
-// 		keys, _, err := iter.ToKVArray(iter.UnionKV(it, it2, -1))
-// 		require.NoError(err)
-// 		require.Equal([][]byte{{1}, {3}, {4}}, keys)
-// 	})
-// 	t.Run("empty both", func(t *testing.T) {
-// 		require := require.New(t)
-// 		tx, _ := db.BeginRw(ctx)
-// 		defer tx.Rollback()
-// 		it, _ := tx.Range(kv.E2AccountsHistory, nil, nil)
-// 		it2, _ := tx.Range(kv.PlainState, nil, nil)
-// 		m := iter.UnionKV(it, it2, -1)
-// 		require.False(m.HasNext())
-// 	})
-// 	t.Run("error handling", func(t *testing.T) {
-// 		require := require.New(t)
-// 		tx, _ := db.BeginRw(ctx)
-// 		defer tx.Rollback()
-// 		it := iter.PairsWithError(10)
-// 		it2 := iter.PairsWithError(12)
-// 		keys, _, err := iter.ToKVArray(iter.UnionKV(it, it2, -1))
-// 		require.Equal("expected error at iteration: 10", err.Error())
-// 		require.Equal(10, len(keys))
-// 	})
-// }
 
 func TestIntersect(t *testing.T) {
 	t.Run("intersect", func(t *testing.T) {
